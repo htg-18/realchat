06c95b68180976d2e6861ce2fa5ac7eb7cf75d3e
Harsh Shah <hs553608@gmail.com>
2024-01-15 23:39:50 +0530

    edit KeywordButtons.jsx

--- client/src/components/KeywordButtons.tsx
+++ client/src/components/KeywordButtons.tsx
@@ -1,29 +1,4 @@
-// interface KeywordButtonsProps {
-//   keywords: string[];
-//   onKeywordClick: (keyword: string) => void;
-// }
 
-// const KeywordButtons: React.FC<KeywordButtonsProps> = ({ keywords, onKeywordClick }) => {
-//   return (
-//     <div className='flex space-x-2 mb-20 '>
-//       {keywords.map((keyword) => (
-//         <button
-//           key={keyword}
-//           className='rounded-[12px] h-8 px-3 hover:bg-zinc-600'
-//           style={{ border: '0.8px solid white' }}
-//           onClick={() => onKeywordClick(keyword)}
-//         >
-//           {keyword}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default KeywordButtons;
-
-
-// KeywordButtons.tsx
 import React from 'react';
 
 interface KeywordButtonsProps {
